66b64236929b71a5baabc6a2b43853bd2b90dda0
Varshashetty26 <120893820+Varshashetty26@users.noreply.github.com>
2024-01-22 00:48:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;//import package for user import
+public class Main
+{
+    static void lastDigit(int num1,int num2){  // Function to check if last digit is same
+        boolean result=num1%10 == num2%10;
+        System.out.print(result);    //if last digit same-->true else-->false
+        }
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int number1=sc.nextInt(); // first number
+	    int number2=sc.nextInt(); //second number
+	    lastDigit(number1,number2);
+	}
+}
