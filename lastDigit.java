67ebec9ae49ff50c33ce3e19d321497b376a0549
Varsha Shetty <varsh@LAPTOP-630OB7MT.(none)')>
2024-02-21 20:18:18 +0530

    done

--- lastDigit.java
+++ lastDigit.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;//import package for user import
-public class Main
+public class Main()
 {
     static void lastDigit(int num1,int num2){  // Function to check if last digit is same
         boolean result=num1%10 == num2%10;
@@ -11,5 +11,6 @@ public class Main
 	    int number1=sc.nextInt(); // first number
 	    int number2=sc.nextInt(); //second number
 	    lastDigit(number1,number2);
+		System.out.println("Hello varsha shetty");
 	}
 }
